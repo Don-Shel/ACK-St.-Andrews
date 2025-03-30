4ec2086390b2995931481dd92489b5365678e1f4
Don-Shel <sheldonletting04@gmail.com>
2025-03-31 00:54:03 +0300

    2

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -179,7 +179,7 @@ export default function ContactPage() {
               </p>
               <div className="h-[400px] rounded-lg overflow-hidden">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7551456978824!2d35.2894493!3d0.4751246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x178101b9a5271bfd%3A0x9d0a1f7cd8dd60ef!2sKibabet!5b1!15sCg9LaWJhYmV0LCBLZW55YZIBD3N1YnVyYmFuX2FyZWFz4AEA!16s%2Fg%2F11c5rg4dxq!5m2!4m1!1i2"
+                  src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d690.212792669055!2d35.30107324074157!3d0.3581283311842563!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2ske!4v1743369252591!5m2!1sen!2ske" 
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
